3512bbf69a04b3ffa3d3ddf38cbd9debeae16465
smassarsky <seth.massarsky@gmail.com>
2020-10-14 17:19:19 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  @@all = []
+
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    self.author != nil ? self.author.name : nil
+  end
+
+end
